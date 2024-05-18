64322de9557a63579963d9b8ddf5d0ed63c3233d
Remi Kujawski <remi.kujawski@gmail.com>
2024-05-18 17:11:47 +0200

    Update.

--- task-2.js
+++ task-2.js
@@ -3,10 +3,3 @@ function formatMessage(message, maxLength){
         return message.slice(0,maxLength)+"...";
     }else return message;
 }
-
-console.log(formatMessage("Curabitur ligula sapien", 16));
-console.log(formatMessage("Curabitur ligula sapien", 23));
-console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
--- task-4.js
+++ task-4.js
@@ -18,10 +18,3 @@ function getShippingCost(country){
     }
     return info;
 }
-
-console.log(getShippingCost("Australia"));
-console.log(getShippingCost("Germany"));
-console.log(getShippingCost("China"));
-console.log(getShippingCost("Chile"));
-console.log(getShippingCost("Jamaica"));
-console.log(getShippingCost("Sweden"));
